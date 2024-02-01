184109dc835f62adfa54e5108fa0f4f5850f8a71
Eduardo Martins <coopasbr@gmail.com>
2024-02-01 15:49:08 -0300

    📂 Episodio #2

---
+++ src/main/java/com/coopas/tutorialspigot/Comandos/FlyC.java
@@ -0,0 +1,40 @@
+package com.coopas.tutorialspigot.Comandos;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class FlyC implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if(cmd.getName().equalsIgnoreCase("fly")) {
+
+            if(sender instanceof Player) {
+                Player p = (Player) sender;
+
+                if(sender.hasPermission("plugin.fly")) {
+
+                    if(p.getAllowFlight() == true) {
+                        p.setAllowFlight(false);
+                        sender.sendMessage("§cSeu fly foi desativado!");
+                    } else {
+                        p.setAllowFlight(true);
+                        sender.sendMessage("§aSeu fly foi ativado!");
+                    }
+
+                } else {
+                    sender.sendMessage("§cVoce nao possui permissao para utilizar este comando!");
+                }
+
+            } else {
+                Bukkit.getConsoleSender().sendMessage("§cO /Fly so pode ser utilizado por jogadores!");
+            }
+        }
+
+
+        return false;
+    }
+}
